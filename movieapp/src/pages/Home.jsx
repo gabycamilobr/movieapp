71d54ced85ce21d09262bb7dc2e80db546fe6ef7
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-27 12:53:41 -0300

    ac01

--- movieapp/src/pages/Home.jsx
+++ movieapp/src/pages/Home.jsx
@@ -4,11 +4,27 @@ function Home() {
     console.log(data)
     return ( 
         <main>
-            <input type="text" name="busca" id="busca" placeholder="Digite aqui sua busca"/>
-            <div className='lista-filmes'>
+            <input type="text" name="busca" id="buscar" placeholder="Digite aqui sua busca"/>
+            <div className='grid grid-cols-3 gap-4'>
                 {
+                    
                 data.map(filme => (
+                    <div className='card bg-slate-400' key={filme.title}>
                     <h1>{filme.title}</h1>
+                    <img src={filme.image} alt={filme.title} />
+
+                    <div className='texto'>
+                        {filme.text.map(texto => (
+                            <p key={texto}>{texto}</p>
+                        ))}
+                    </div>
+                    
+                    <div className='tag'>
+                        {filme.tags.map(tag => (
+                            <span key={tag} className='text-purple-600'> {tag} </span>
+                        ))}
+                    </div>
+                    </div>
                 ))}
             </div>
         </main>
